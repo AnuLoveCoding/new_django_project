88182c4d0fec5e311d74725af84b81a990f4b71d
anuLoveCoading <anusrivas07@gmail.com>
2023-03-22 01:01:36 +0530

    python_project

--- new_django_project/views.py
+++ new_django_project/views.py
@@ -8,5 +8,5 @@ def course(request):
     return HttpResponse("<b>Contact-Us :- anurag.itmatters@gmail.com</b>")
 
 def mobile(request):
-    return HttpResponse("My mobile number is 8595848097")
+    return HttpResponse("<u><b>My mobile number is : 8595848097</b></u>")
 
